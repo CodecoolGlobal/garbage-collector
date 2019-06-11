2f0d1a8ac5de98290e0c2508da56f8c987965bd8
Liz <pachaffy@gmail.com>
2019-06-11 11:04:59 +0200

    Implement Coordinate

---
+++ src/main/java/com/codecool/garbagecollector/model/Coordinate.java
@@ -0,0 +1,27 @@
+package com.codecool.garbagecollector.model;
+
+import javax.persistence.Embeddable;
+
+@Embeddable
+public class Coordinate {
+
+    private double longitude;
+
+    private double latitude;
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+}
