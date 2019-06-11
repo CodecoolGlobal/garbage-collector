32b7c15126b075f1f8d79a33ad1a8df2aadd3bfb
Liz <pachaffy@gmail.com>
2019-06-11 11:05:20 +0200

    Implement Location

---
+++ src/main/java/com/codecool/garbagecollector/model/Location.java
@@ -0,0 +1,74 @@
+package com.codecool.garbagecollector.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+@Entity
+public class Location {
+
+    public Location() {
+        stock = new ArrayList<>();
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String name;
+
+    @OneToMany(mappedBy = "location")
+    private List<Garbage> stock;
+
+    @Embedded
+    private Address address;
+
+    private String phoneNumber;
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Garbage> getStock() {
+        return stock;
+    }
+
+    public void setStock(List<Garbage> stock) {
+        this.stock = stock;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public void addGarbage(Garbage garbage) {
+        garbage.setLocation(this);
+        stock.add(garbage);
+    }
+}
