b8c2627fdd42892b6f7da390fb574e2acb1f6a21
Liz <pachaffy@gmail.com>
2019-06-11 11:04:23 +0200

    Implement Garbage

--- src/main/java/com/codecool/garbagecollector/model/Garbage.java
+++ src/main/java/com/codecool/garbagecollector/model/Garbage.java
@@ -1,4 +1,74 @@
 package com.codecool.garbagecollector.model;
 
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+@Entity
 public class Garbage {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Embedded
+    private Type type;
+
+    private int quantity;
+
+    @ManyToOne
+    private Location location;
+
+    @ManyToOne
+    private Status status;
+
+    private String description;
+
+
+    public long getId() {
+        return id;
+    }
+
+    public Type getType() {
+        return type;
+    }
+
+    public void setType(Type type) {
+        this.type = type;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public Location getLocation() {
+        return location;
+    }
+
+    public void setLocation(Location location) {
+        this.location = location;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
