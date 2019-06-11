ca39a0ed120e693e6530e2360253f48b9ceba0b1
mszotek <mszotek@gmail.com>
2019-06-12 00:09:32 +0200

    Implement getLocation for 3 parameters

--- src/main/java/com/codecool/garbagecollector/service/LocationService.java
+++ src/main/java/com/codecool/garbagecollector/service/LocationService.java
@@ -1,30 +1,50 @@
 package com.codecool.garbagecollector.service;
 
 import com.codecool.garbagecollector.model.Address;
-import com.codecool.garbagecollector.model.Garbage;
 import com.codecool.garbagecollector.model.Location;
-import com.codecool.garbagecollector.model.Type;
 
 import javax.persistence.EntityManager;
 import javax.persistence.TypedQuery;
 import javax.persistence.criteria.*;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
 public class LocationService {
 
-    public void getLocationByParameters(EntityManager em){
+    public void getLocationByParameters(EntityManager em, Map<String, String[]> queryParams) {
         CriteriaBuilder cb = em.getCriteriaBuilder();
         CriteriaQuery<Location> q = cb.createQuery(Location.class);
         Root<Location> locationRoot = q.from(Location.class);
+        Join<Location, Address> addressJoin = locationRoot.join("address", JoinType.LEFT);
 
-        Join<Garbage, Address> addressJoin = locationRoot.join("address", JoinType.LEFT);
+        List<Predicate> predicates = new ArrayList<>();
 
-        q.where(
-                cb.or(
-                        cb.equal(addressJoin.get("city"), "Krakow"),
-                        cb.equal(addressJoin.get("city"), "Poznan")
-                ));
+        List<Predicate> idPredicates = new ArrayList<>();
+        if (queryParams.containsKey("id")) {
+            for (String idParam : queryParams.get("id")) {
+                idPredicates.add(cb.equal(locationRoot.get("id"), idParam));
+            }
+            predicates.add(cb.or(idPredicates.toArray(new Predicate[]{})));
+        }
+
+        List<Predicate> namePredicates = new ArrayList<>();
+        if (queryParams.containsKey("name")) {
+            for (String nameParam : queryParams.get("name")) {
+                namePredicates.add(cb.equal(locationRoot.get("name"), nameParam));
+            }
+            predicates.add(cb.or(namePredicates.toArray(new Predicate[]{})));
+        }
+
+        List<Predicate> cityPredicates = new ArrayList<>();
+        if (queryParams.containsKey("city")) {
+            for (String cityParam : queryParams.get("city")) {
+                cityPredicates.add(cb.equal(addressJoin.get("city"), cityParam));
+            }
+            predicates.add(cb.or(cityPredicates.toArray(new Predicate[]{})));
+        }
+
+        q.where(predicates.toArray(new Predicate[]{}));
 
         CriteriaQuery<Location> select = q.select(locationRoot);
 
@@ -32,8 +52,8 @@ public class LocationService {
 
         List<Location> results = query.getResultList();
 
-        for (Location location:
-             results) {
+        for (Location location :
+                results) {
             System.out.println(location.getName());
         }
     }
