6d15cf7c36e511175ccdc1a21a069d32e9f78ae0
Liz <pachaffy@gmail.com>
2019-06-13 10:00:49 +0200

    Add Garbage Service to Garbage Servlet

--- src/main/java/com/codecool/garbagecollector/controller/GarbageServlet.java
+++ src/main/java/com/codecool/garbagecollector/controller/GarbageServlet.java
@@ -1,6 +1,9 @@
 package com.codecool.garbagecollector.controller;
 
-import com.codecool.garbagecollector.service.UtilityService;
+import java.util.List;
+import java.util.Map;
+import java.io.IOException;
+
 import com.google.gson.Gson;
 
 import javax.servlet.ServletException;
@@ -8,24 +11,28 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
 
+import com.codecool.garbagecollector.model.Garbage;
+import com.codecool.garbagecollector.service.GarbageService;
+import com.codecool.garbagecollector.service.UtilityService;
 
 @WebServlet(urlPatterns = {"/garbage/*"})
 public class GarbageServlet extends HttpServlet {
 
+    private GarbageService garbageService;
+
+    public GarbageServlet() {
+        garbageService = new GarbageService();
+    }
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        Map<String, String[]> queryParams = UtilityService.getQueryParameters(req);
-
         Gson gson = new Gson();
-        resp.getWriter().write(gson.toJson(queryParams));
+        Map<String, String[]> queryParams = UtilityService.getQueryParameters(req);
+        List<Garbage> stock = garbageService.getStockBy(queryParams);
+        resp.getWriter().write(gson.toJson(stock));
     }
-
-
-    }
+}
 
 
 
